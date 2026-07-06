5facecbff51e383e8e89ebb87f91a02e011a08d1
Japjot Singh <singh.japjot11@gmail.com>
2026-07-06 13:31:53 +0200

    init

---
+++ fallstudie-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fallstudie.app',
+  appName: 'Fallstudie App',
+  webDir: 'dist'
+};
+
+export default config;
